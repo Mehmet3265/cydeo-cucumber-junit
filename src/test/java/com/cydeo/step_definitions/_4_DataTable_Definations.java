fddbd53a14eb4a5568833e1a213534ea910e9e99
Mehmet3265 <mehmetfurkangulgor@gmail.com>
2022-10-17 00:00:22 +0300

    updated cucumber

--- src/test/java/com/cydeo/step_definitions/_4_DataTable_Definations.java
+++ src/test/java/com/cydeo/step_definitions/_4_DataTable_Definations.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class _4_DataTable_Definations {
     @Then("user should see fruits I like")
-    public void user_should_see_fruits_ı_like(List<String> fruits) {
+    public void user_should_see_fruits_i_like(List<String> fruits) {
         System.out.println(fruits);
         System.out.println(fruits.get(1));
     }
